cd577d9edca6558084153f2f1e07bb6e4f9040b1
Thiago <thiago@localhost.localdomain>
2024-05-22 11:30:57 -0300

    Servidor Configurado

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log("Ah!");
+require('dotenv').config()
+const express = require('express')
+
+const app = express()
+
+app.use(express.json())
+
+app.get('/', async () => {
+    return res.json({ mensagem: 'Bigorna Inflável não Afunda' })
+})
+
+app.listen(process.env.PORT)
